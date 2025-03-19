262c303eb7a5de8cab8776a41c6a4a5c86e2cf20
Pedro-HD-Cruz <pedrohdcruz05@gmail.com>
2025-03-19 16:53:30 -0300

    Realizada criação das 3 telas e união do front com o back

--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,3 +1,7 @@
+using Microsoft.Maui.Controls;
+using MauiAppMinhasCompras.Models;
+using System.Threading.Tasks;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +10,23 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Produto p = new Produto
+            {
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
+            await App.Db.Insert(p);
+            await DisplayAlert("Sucesso", "Produto inserido com sucesso", "OK");
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Erro", ex.Message, "OK");
+        }
+    }
 }

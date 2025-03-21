fea25916bc7f405ad3c2c82f66f1a2ef7cc84d41
Pedro-HD-Cruz <pedrohdcruz05@gmail.com>
2025-03-21 16:26:29 -0300

    validação da inserção de produtos

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,11 +4,47 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        string _descricao;
+        double _quantidade;
+        double _preco;
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
-        public double Quantidade { get; set; }
-        public double Preco { get; set; }
+        public string Descricao
+        {
+            get => _descricao;
+            set
+            {
+                if(value == null || value == string.Empty)
+                {
+                    throw new Exception("Descrição não pode ser vazia");
+                }
+                _descricao = value;
+            }
+        }
+        public double Quantidade
+        {
+            get => _quantidade;
+            set 
+            {
+                if (value == null || value == 0)
+                {
+                    throw new Exception("Quantidade não pode ser vazia");
+                }
+                _quantidade = value;
+            }
+        }
+        public double Preco
+        {
+            get => _preco;
+            set
+            {
+                if (value == null || value == 0)
+                {
+                    throw new Exception("Preço não pode ser vazio");
+                }
+                _preco = value;
+            }
+        }
         public double Total { get => Quantidade * Preco; }
     }
 }
